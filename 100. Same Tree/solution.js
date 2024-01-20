5b15ac3727d4cacb0ce7fc46611550b04688505a
joelpatel <joelpatel.cs@gmail.com>
2024-01-19 20:50:05 -0500

    100. Same Tree

---
+++ 100. Same Tree/solution.js
@@ -0,0 +1,17 @@
+function isNil(element) {
+    if (element === undefined || element === null) {
+        return true;
+    }
+}
+
+var isSameTree = function(p, q) {
+    if (isNil(p) === true && isNil(q) === true) {
+        return true;
+    }
+
+    if (isNil(p) || isNil(q) || p.val !== q.val) {
+        return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
